d897628c7fa4df919cc090e4ba2784277b27f50c
Nadim <nadimrifaii3@gmail.com>
2023-09-03 19:12:20 +0300

    5ara

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import Test from './routes/test-component/test.component';
 import Navigation from './components/navigation/navigation.component';
+import { Routes, Route } from 'react-router-dom'
+im
 function App() {
   return (
-    <Navigation />
+    <Routes  >
+      <Route path='/' element={<Navigation />}>
+      </Route>
+    </Routes>
   );
 }
 export default App;
